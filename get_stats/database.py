9d18dda9d1aed75c610e5050017fe85fd6a8e748
Piotr Sztela <piotr.sztela@hotmail.com>
2021-11-06 18:09:59 +0100

    add requests and dotenv library to requirements

--- get_stats/database.py
+++ get_stats/database.py
@@ -5,7 +5,7 @@ from .models import Malaga
 from dotenv import load_dotenv
 
 
-def add_to_database():
+def prepare_data() -> list:
     load_dotenv("./.env")
     url_correct = "https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/fechaini/2019-01-01T00:00:00UTC/fechafin/2021-09-30T23:59:59UTC/estacion/6156X/"
     querystring = {"api_key": os.environ["API_KEY"]}
@@ -15,21 +15,24 @@ def add_to_database():
 
     data = json.loads(response.text)
 
-    url_datos = data["datos"]
+    url_data = data["datos"]
 
-    response_datos = requests.request("GET", url_datos)
-    datos_final = json.loads(response_datos.text)
+    response_data = requests.request("GET", url_data)
+    data_final = json.loads(response_data.text)
+
+    return data_final
 
     # data_test = {'fecha': '2019-01-01', 'indicativo': '6156X', 'nombre': 'MÁLAGA, CENTRO METEOROLÓGICO', 'provincia': 'MALAGA', 'altitud': '54', 'tmed': '14,0', 'prec': '0,0', 'tmin': '6,1', 'horatmin': '05:00', 'tmax': '21,9', 'horatmax': '14:00', 'dir': '31', 'velmedia': '1,9', 'racha': '4,4', 'horaracha': '02:50', 'presMax': '1027,7', 'horaPresMax': 'Varias', 'presMin': '1024,0', 'horaPresMin': '14'}
 
-    for data in datos_final:
-        for key, value in data.items():
+def add_to_database(data: list, city) -> None:
+    for entry in data:
+        for key, value in entry.items():
             try:
                 value = float(value.replace(",", "."))
-                data.update({key: value})
+                entry.update({key: value})
             except:
                 pass
             if isinstance(value, str) and value.lower() == "varias":
                 value = None
-                data.update({key: value})
-        Malaga.objects.update_or_create(**data)
+                entry.update({key: value})
+        city.objects.update_or_create(**entry)
